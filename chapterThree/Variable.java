f7d104df8ee35bfafab91a5c1b53f0c8507bdc84
Rohit Bhardwaj <rohitbhardwajindia@gmail.com>
2022-03-14 14:37:25 +0530

    first commit

---
+++ chapterThree/Variable.java
@@ -0,0 +1,8 @@
+class Variable {
+    public static void main(String[] args) {
+        double x = 24.889;
+        byte b = (byte)x;
+        System.out.println(x);
+        System.out.println(b);
+    }
+}
